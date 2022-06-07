b88fe25a03cd69687028d55146c04829f3131997
Matheus diogenes <matheusdiogenes98@gmail.com>
2022-06-07 15:01:14 -0300

    module import fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
-//1:49:00
+
 @Module({
   imports: [
     ConfigModule.forRoot({
